091b2b674de39ca69802cd01245b6517c2089ef7
Dylan Cordero Mena <dsoultime@gmail.com>
2024-06-30 22:54:00 -0600

    ok

--- proyectofinal/src/App.js
+++ proyectofinal/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Menu from './misc/Menu';
@@ -9,18 +9,31 @@ import ArtistList from './Artist/ArtistList';
 import PurchaseList from './Purchase/PurchaseList';
 
 function App() {
+  const [activeComponent, setActiveComponent] = useState(''); // Estado para el componente activo
+
+  // Función para renderizar el componente activo
+  const renderActiveComponent = () => {
+    switch (activeComponent) {
+      case 'User':
+        return <UserList />;
+      case 'Album':
+        return <AlbumList />;
+      case 'Artist':
+        return <ArtistList />;
+      case 'Purchase':
+        return <PurchaseList />;
+      default:
+        return <div>Seleccione una opción del menú</div>;
+    }
+  };
+
   return (
     <div className="container-fluid">
       <div className="container-fluid">
-        <Menu />
+        <Menu activeComponent={activeComponent} setActiveComponent={setActiveComponent} />
       </div>
       <div className="container-fluid">
-
-        <UserList />
-        <AlbumList /> 
-        <ArtistList />
-        <PurchaseList/>
-        
+        {renderActiveComponent()}
       </div>
       <div className="container-fluid">
         <Footer />
